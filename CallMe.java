824bf8e75e09c44d40f8c87835d4ce4e5b34684e
avnisingh07 <157941574+avnisingh07@users.noreply.github.com>
2024-04-24 23:40:45 +0530

    Add files via upload

---
+++ CallMe.java
@@ -0,0 +1,15 @@
+package ASSIGNMENT_9;
+
+public class CallMe {
+
+    synchronized public void call(String msg) {
+
+        System.out.print("[ "+msg);
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+        }
+        System.out.println(" ]");
+    }
+
+}
